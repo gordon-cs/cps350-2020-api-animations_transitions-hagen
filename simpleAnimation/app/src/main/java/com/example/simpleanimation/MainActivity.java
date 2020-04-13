b5704a7f11dee0f3229011f5b8eb1a59344e86b9
f4hagen <ffhagen@gmail.com>
2020-04-13 06:21:41 -0400

    adding button animation

--- simpleAnimation/app/src/main/java/com/example/simpleanimation/MainActivity.java
+++ simpleAnimation/app/src/main/java/com/example/simpleanimation/MainActivity.java
@@ -5,9 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.drawable.AnimatedImageDrawable;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button btn;
+
     AnimationDrawable wifiAnimation;
 
     @Override
@@ -15,9 +23,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //https://developer.android.com/guide/topics/graphics/drawable-animation
         ImageView imageView = (ImageView)findViewById(R.id.image);
         imageView.setBackgroundResource(R.drawable.animation);
         wifiAnimation = (AnimationDrawable) imageView.getBackground();
+
+        //https://developer.android.com/guide/topics/graphics/view-animation?hl=tr
+        btn=(Button)findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation animation=AnimationUtils.loadAnimation(MainActivity.this,R.anim.mixed_anim);
+
+                btn.startAnimation(animation);
+            }
+        });
 
     }
 
