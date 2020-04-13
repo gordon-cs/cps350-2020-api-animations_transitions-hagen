9cfd1144b575c9fbbc77360f2d470ae4c0523d3b
f4hagen <ffhagen@gmail.com>
2020-04-13 05:23:44 -0400

    adding animation demo

--- simpleAnimation/app/src/main/java/com/example/simpleanimation/MainActivity.java
+++ simpleAnimation/app/src/main/java/com/example/simpleanimation/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.simpleanimation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.AnimatedImageDrawable;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    AnimationDrawable wifiAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = (ImageView)findViewById(R.id.image);
+        imageView.setBackgroundResource(R.drawable.animation);
+        wifiAnimation = (AnimationDrawable) imageView.getBackground();
+
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        wifiAnimation.start();
     }
 }
